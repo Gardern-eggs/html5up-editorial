de61d54d4bbffb77f97d113621103030d2472139
Gardern-eggs <sean.mensah@amalitech.org>
2022-07-14 11:05:02 +0100

    learn more button removed

--- assets/js/main.js
+++ assets/js/main.js
@@ -31,15 +31,7 @@
 				}, 100);
 			});
 // learn more button
-const parent=document.querySelector('.content');
-parent.addEventListener('click',event=>{
-	const current=event.target;
-	const readbtn=current.className.includes('learnMore');
-	if(!readbtn)return;
-	const currentText=event.textContent.includes('learn More') ? "learn More":"learn more";
 
-
-})
 
 
 		// ... stopped resizing.
